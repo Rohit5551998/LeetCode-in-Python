8f128b42d60a0e464a6379ad8e01de867beb44e1
Rohit Chawhan <40637586+Rohit5551998@users.noreply.github.com>
2021-09-13 07:37:44 +0000

    Single Number Done

--- Math/Single Number.py
+++ Math/Single Number.py
@@ -1,2 +1,11 @@
 from typing import List
 
+nums =  [1, 2, 3, 4, 1, 2, 3, 4, 5]
+
+def singleNumber(nums: List[int]) -> int:
+	unqNumsSum = sum(set(nums))
+	numSum = sum(nums)
+
+	return 2*unqNumsSum - numSum
+
+print(f"The number that appers only once is {singleNumber{nums}}")
